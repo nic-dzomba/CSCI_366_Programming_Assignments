c4c34592f583cfd7f9827dd702375b1bfed199f2
nic-dzomba <nicustm@gmail.com>
2020-03-11 20:43:12 -0600

    Update Server.cpp
    start fixing things for testing

--- src/Server.cpp
+++ src/Server.cpp
@@ -17,6 +17,9 @@
 
 #include "common.hpp"
 #include "Server.hpp"
+#include <fstream>
+#include <iomanip>
+#include <cereal/archives/json.hpp>
 
 
 /**
@@ -28,9 +31,9 @@
 
 //code found from https://codeyarns.com/2018/02/02/how-to-find-size-of-file-in-c/
 int get_file_length(ifstream *file){
-//    std::ifstream in_file(file, std::ios::binary | std::ios::ate);
-//    int file_size = in_file.tellg();
-//    return file_size;
+    file->seekg(0, std::ios::end);
+    int file_size = file->tellg();
+    return file_size;
 }
 
 
@@ -39,122 +42,131 @@ char p1array[BOARD_SIZE][BOARD_SIZE];
 char p2array[BOARD_SIZE][BOARD_SIZE];
 
 
+class json;
+
 void Server::initialize(unsigned int board_size,
                         string p1_setup_board,
                         string p2_setup_board){
 
-//    std::ifstream p1file(p1_setup_board);
-//    std::ifstream p2file(p2_setup_board);
-//
-//    if (!p1file.is_open()) {
-//        throw "player 2 board not found";
-//    }
-//
-//    if (!p2file.is_open()) {
-//        throw "player 2 board not found";
-//    }
-//    std::string str;
-//
-//    int counter = 0;
-//
-//    while(std::getline(p1file, str)) {
-//        std::cout << str << "\n";
-//
-//        if (str.length() == board_size) {
-//            for (int i = 0; i < sizeof(str); i++)
-//                p1array[counter][i] = str[i];
-//            counter += 1;
-//        } else {
-//            throw  "Player 1 Board has an incorrect line";
-//        }
-//    }
-//
-//    if (counter != board_size + 1) {
-//        throw "Player 1 Board has incorrect length";
-//    }
-//
-//    counter = 0;
-//
-//    while(std::getline(p2file, str)) {
-//        std::cout << str << "\n"
-//
-//        if (str.length() == board_size) {
-//            for (int i = 0; i < sizeof(str); i++)
-//                p2array[counter][i] = str[i];
-//            counter += 1;
-//        } else {
-//            throw  "Player 2 Board has an incorrect line";
-//        }
-//    }
-//
-//    if (counter != board_size + 1) {
-//        throw "Player 2 Board has incorrect length";
-//    }
+    Server::board_size = board_size;
+
+    std::ifstream p1file(p1_setup_board);
+    std::ifstream p2file(p2_setup_board);
+
+    if (!p1file.is_open()) {
+        throw "player 2 board not found";
+    }
+
+    if (!p2file.is_open()) {
+        throw "player 2 board not found";
+    }
+    std::string str;
+
+    int counter = 0;
+
+    while(std::getline(p1file, str)) {
+        std::cout << str << "\n";
+
+        if (str.length() == board_size) {
+            for (int i = 0; i < sizeof(str); i++)
+                p1array[counter][i] = str[i];
+            counter += 1;
+        } else {
+            throw  "Player 1 Board has an incorrect line";
+        }
+    }
+
+    if (counter != board_size ) {
+        throw "Player 1 Board has incorrect length";
+    }
+
+    counter = 0;
+
+    while(std::getline(p2file, str)) {
+        std::cout << str << "\n";
+
+        if (str.length() == board_size) {
+            for (int i = 0; i < sizeof(str); i++)
+                p2array[counter][i] = str[i];
+            counter += 1;
+        } else {
+            throw  "Player 2 Board has an incorrect line";
+        }
+    }
+
+    if (counter != board_size ) {
+        throw "Player 2 Board has incorrect length";
+    }
 
 }
 
 
 int Server::evaluate_shot(unsigned int player, unsigned int x, unsigned int y) {
 
-//    if ((player < 1) || (player > 2)) {
-//        throw "player number out of bounds";
-//    }
-//
-//    if ((x > board_size) || (y > board_size)) {
-//        return OUT_OF_BOUNDS;
-//    }
-//
-//    if (player == 1) {
-//        if (p2array[x][y] == "_") {
-//            return MISS;
-//        } else {
-//            return HIT;
-//        }
-//    }
-//
-//    if (player == 2) {
-//        if (p1array[x][y] == "_") {
-//            return MISS;
-//        } else {
-//            return HIT;
-//        }
-//    }
+    if ((player < 1) || (player > 2)) {
+        throw "player number out of bounds";
+    }
+
+    printf("%d, %d, %d\n", x, y, BOARD_SIZE);
+
+    if ((x > (BOARD_SIZE - 1)) || (y > (BOARD_SIZE - 1))) {
+        return OUT_OF_BOUNDS;
+    }
+
+    if (player == 1) {
+        if (p2array[x][y] == '_') {
+            return MISS;
+        } else {
+            return HIT;
+        }
+    }
+
+    if (player == 2) {
+        if (p1array[x][y] == '_') {
+            return MISS;
+        } else {
+            return HIT;
+        }
+    }
 
 }
 
 
 int Server::process_shot(unsigned int player) {
-//    if ((player < 1) || (player > 2)) {
-//        throw "player number out of bounds";
-//    }
-//
-//    using json = nlohmann::json;
-//    json j;
-//    std::ifstream shot_data("player_%d.shot.json", player);
-//
-//    if (!shot_data.is_open()) {
-//        return NO_SHOT_FILE;
-//    }
-//
-//    shot_data >> j;
-//    shot_data.close();
-//
-//    if (j.find("x") != j.end()) {
-//        int x = j["x"];
-//    }
-//
-//    if (j.find("y") != j.end()) {
-//        int y = j["y"];
-//    }
-//
-//    int result = evaluate_shot(player, x, y);
-//
-//    json result_json = {
-//            {"result", result}
-//    };
-//
-//    std::ofstream oput("player_%d.result.json", player);
-//    oput << std::setw(4) << result_json << std::endl;
-//
-//    return SHOT_FILE_PROCESSED;
+
+    if ((player < 1) || (player > 2)) {
+        throw "player number out of bounds";
+    }
+
+    using json = nlohmann::json;
+    json j;
+    std::ifstream shot_data("player_%d.shot.json", player);
+
+    if (!shot_data.is_open()) {
+        return NO_SHOT_FILE;
+    }
+
+    shot_data >> j;
+    shot_data.close();
+
+    int x;
+    if (j.find("x") != j.end()) {
+        x = j["x"];
+    }
+
+    int y;
+    if (j.find("y") != j.end()) {
+        y = j["y"];
+    }
+
+    int result = evaluate_shot(player, x, y);
+
+    json result_json = {
+            {"result", result}
+    };
+
+    std::ofstream oput("player_%d.result.json", player);
+    oput << std::setw(4) << result_json << std::endl;
+
+    return SHOT_FILE_PROCESSED;
 }
